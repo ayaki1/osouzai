1e892b65a633ebc01d2bd249c8f5d5096e899efc
Ayaki <ayaki067@gmail.com>
2023-02-04 16:29:18 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-// -----------------------------------------------------------------------------
-// モジュールのインポート
-const server = require("express")();
-const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
+'use strict';
 
-// -----------------------------------------------------------------------------
-// パラメータ設定
-const line_config = {
-    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
-    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 3000;
+
+const config = {
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
 };
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-server.listen(process.env.PORT || 3000);
+const app = express();
 
+app.get('/', (req, res) => res.send('Hello LINE BOT!(GET)')); //ブラウザ確認用(無くても問題ない)
+app.post('/webhook', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
 
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200);
-    console.log(req.body);
+    //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
+    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
+        res.send('Hello LINE BOT!(POST)');
+        console.log('疎通確認用');
+        return; 
+    }
+
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
 });
+
+const client = new line.Client(config);
+
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text //実際に返信の言葉を入れる箇所
+  });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
